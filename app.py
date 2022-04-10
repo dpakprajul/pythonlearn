cdcbb93437881f83e3ef5032690105b5ac046734
Deepak <bindaaspratiks@gmail.com>
2022-04-10 18:38:50 +0200

    pytorch linear regression

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+st.write("""
+#Explore different ML methods""")
